62fa6a1f88310155eb3bc83612a208dc83945e4a
Louis Cospain <elcoosp@gmail.com>
2019-04-11 22:48:24 +0200

    refactor(docs): split functions by files and add doc comments

---
+++ packages/primo/asyncStateSlice.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+/**
+ * @typedef {object} AsyncStateSliceObject
+ * @property  {array} errors=Array An array of errors
+ * @property  {boolean} isSuccess=false Success status
+ * @property  {boolean} isPending=false Pending status
+ * @property  {boolean} isFailure=false Failure status
+ */
+
+/**
+ * @param {AsyncStateSliceObject} slice
+ * @returns {AsyncStateSliceObject}
+ */
+export default function asyncStateSlice (
+  { errors, isSuccess, isPending, isFailure } = {
+    errors: [],
+    isSuccess: false,
+    isPending: false,
+    isFailure: false
+  }
+) {
+  return {
+    errors,
+    isSuccess,
+    isFailure,
+    isPending
+  }
+}
---
+++ packages/primo/entityStateSlice.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/**
+ * @typedef {object} EntityStateSlice
+ * @property  {array} allIds=Array All of the entity ids, for ordering
+ * @property  {object} meta=Object Entity related informations
+ * @property  {object} byId=false An object for quick id retrieval of entities
+ */
+
+/**
+ * @param {EntityStateSlice} slice
+ * @returns {EntityStateSlice}
+ */
+export default function entityStateSlice (
+  { byId, allIds, meta } = { byId: {}, allIds: [], meta: {} }
+) {
+  return { byId, allIds, meta }
+}
